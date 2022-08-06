e19e296b92bdf98bb8c30f65820dcf154ac86024
AGMarasco <alexmarasco13@gmail.com>
2022-08-06 16:15:16 -0400

    unused scroll function in App to scroll to selector

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,15 +2,22 @@
 import Page from './page';
 
 import './App.scss';
+import { useEffect } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-        <Page pageId={1}/>
-        <Page pageId={2}/>
-        <Page pageId={3}/>
-    </div>
-  );
+
+    const scroll = (selector) => {
+        const section = document.querySelector(selector);
+        section.scrollIntoView( { behavior: 'smooth', block: 'start' } );
+    };
+    
+    return (
+        <div className="App">
+            <Page pageId='page1'/>
+            <Page pageId='page2'/>
+            <Page pageId='page3'/>
+        </div>
+    );
 }
 
 export default App;
