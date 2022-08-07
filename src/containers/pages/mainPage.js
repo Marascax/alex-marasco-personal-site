a17b54237a8adfaad530acfb1585e76a05582eac
AGMarasco <alexmarasco13@gmail.com>
2022-08-07 11:54:59 -0400

    mainPage two-column format, image and text

--- src/containers/pages/mainPage.js
+++ src/containers/pages/mainPage.js
@@ -7,17 +7,16 @@ const MainPage = props => {
     return (
         <div className="MainPage">
 
-            <div className="Header">
-                <h1>Alexander Marasco</h1>
-            </div>
-
             <div className="MainContent">
 
                 <div className="SelfImage">
                     <img src="images/Alex_Marasco-Crop.png"/>
                 </div>
-                
 
+                <div className="WelcomText">
+                    <h1>Alexander Marasco</h1>
+                </div>
+                
             </div>
 
         </div>
