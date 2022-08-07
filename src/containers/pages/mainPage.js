f19a9e730af59fa3de3037e973befc9772a86926
AGMarasco <alexmarasco13@gmail.com>
2022-08-07 11:27:35 -0400

    change import to basePage

--- src/containers/pages/mainPage.js
+++ src/containers/pages/mainPage.js
@@ -1,10 +1,14 @@
 // opening page that shows up at start
 
+import './mainPage.scss';
+
 const MainPage = props => {
 
     return (
-        <div>
-
+        <div className="MainPage">
+            <div className="Header">
+                <h1>Alexander Marasco</h1>
+            </div>
         </div>
     )
 }
