65780169c4dc464af187bcbaeed4dd5e9f6613ec
AGMarasco <alexmarasco13@gmail.com>
2022-08-07 21:09:54 -0400

    Revert "Merge branch 'feature/context'"
    This reverts commit 3fb57550a455d71f5bcc4cedc7aebafe352c8cbc, reversing
    changes made to b8d17016dbf00b959debf12506bd496880104288.

--- src/containers/App.js
+++ src/containers/App.js
@@ -10,63 +10,56 @@ import './App.scss';
 function App() {
     const context = useAppContext();
     const appRef = useRef(null);
-    const currPageIndex = useRef(0);
 
     const pages = ['page1', 'page2', 'page3'];
+    let currentPageIndex;
 
-    // const updatePageSessionStorage = () => context.setCurrentPage(currPageIndex.current);
+    const updatePageSessionStorage = () => setSessionStorageItem('currentPage', currentPageIndex);
 
-    // const scrollUp = () => {
-    //     context.scrollTo(`#${pages[--currPageIndex.current]}`);
-    //     updatePageSessionStorage();
-    // }
+    const scrollUp = () => {
+        context.scroll(`#${pages[--currentPageIndex]}`);
+        updatePageSessionStorage();
+    }
 
-    // const scrollDown = () => {
-    //     context.scrollTo(`#${pages[++currPageIndex.current]}`);
-    //     updatePageSessionStorage();
-    // }
+    const scrollDown = () => {
+        context.scroll(`#${pages[++currentPageIndex]}`);
+        updatePageSessionStorage();
+    }
 
     const wheelHandler = e => {
         e.preventDefault();
 
-        currPageIndex.current = context.getCurrentPage();
-
         if (e.deltaY < 0) { // scroll up
             // can't scroll up any farther if at top page
-            if (currPageIndex.current > 0) {
-                context.scrollUp();
+            if (currentPageIndex > 0) {
+                scrollUp();
             }
         } else if (e.deltaY > 0) { // scroll down
             // cant scroll down if at bottom page
-            if (currPageIndex.current !== pages.length - 1) {
-                context.scrollDown();
+            if (currentPageIndex != pages.length - 1) {
+                scrollDown();
             }
         }
     }
 
     // when app mounts
     useEffect(() => {
-        // once mounted get app element and add wheel handler
-        let currentRef = null;
+        appRef.current.addEventListener('wheel', wheelHandler, { passive: false });
 
-        if (appRef.current) {
-            currentRef = appRef.current;
-            currentRef.addEventListener('wheel', wheelHandler, { passive: false });
+        // get the sessionstorage for the current page index
+        // + to convert into number
+        currentPageIndex = +getSessionStorageItem('currentPage')
+        // if null, set and save 0
+        if (!currentPageIndex) {
+            currentPageIndex = 0;
+            updatePageSessionStorage();
         }
-
-        // add the current pages to our context
-        context.updatePages(pages);
-
-        currPageIndex.current = context.getCurrentPage();
-
         // scroll to current page
-        // context pages state may not have updated yet, so scroll to page with manual selector
-        context.scrollTo(`#${pages[currPageIndex.current]}`);
+        context.scroll(`#${pages[currentPageIndex]}`);
 
         // when app unmounts
         return () => {
-            if (currentRef)
-                currentRef.removeEventListener('wheel', wheelHandler, { passive: false });
+            appRef.current.removeEventListener('wheel', wheelHandler, { passive: false });
         }
     }, []);
 
