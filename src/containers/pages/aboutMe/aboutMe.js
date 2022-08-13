36233533cb8a53a4742575b5828f3f903074eaf2
AGMarasco <alexmarasco13@gmail.com>
2022-08-13 17:55:40 -0400

    about me text file

--- src/containers/pages/aboutMe/aboutMe.js
+++ src/containers/pages/aboutMe/aboutMe.js
@@ -3,10 +3,17 @@ import { useAppContext } from '../../../lib/appContext';
 import './aboutMe.scss';
 
 const AboutMe = props => {
+
+    const context = useAppContext();
+
     return (
         <div className='AboutMe'>
             <div className='Header'>
                 <h1>About Me</h1>
+            </div>
+
+            <div className='MainContent'>
+
             </div>
         </div>
     );
