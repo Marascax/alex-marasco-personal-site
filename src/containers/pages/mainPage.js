0082154c6f2928b88d9f0212600b866c0c1bbc6d
AGMarasco <alexmarasco13@gmail.com>
2022-08-13 14:27:07 -0400

    scroll down clicking mainPage arrow

--- src/containers/pages/mainPage.js
+++ src/containers/pages/mainPage.js
@@ -1,9 +1,13 @@
 // opening page that shows up at start
 import DownArrow from '../../components/downArrow';
 
+import { useAppContext } from '../../lib/appContext';
+
 import './mainPage.scss';
 
 const MainPage = props => {
+
+    const context = useAppContext();
 
     return (
         <div className="MainPage">
@@ -18,7 +22,7 @@ const MainPage = props => {
                     <p>Welcome to the Personal Site of</p>
                     <h1>Alexander Marasco</h1>
                     
-                    <div className='DownArrow'>
+                    <div className='DownArrow' onClick={() => context.scrollDown()}>
                         <DownArrow width="100"/>
                     </div>
                     
