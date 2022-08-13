36473f5946d07e1ac02f8fcf8d6249af3d0ba7c6
AGMarasco <alexmarasco13@gmail.com>
2022-08-13 14:41:45 -0400

    init styling, added page to App

---
+++ src/containers/pages/aboutMe/aboutMe.js
@@ -0,0 +1,15 @@
+import { useAppContext } from '../../../lib/appContext';
+
+import './aboutMe.scss';
+
+const AboutMe = props => {
+    return (
+        <div className='AboutMe'>
+            <div className='MainContent'>
+
+            </div>
+        </div>
+    );
+}
+
+export default AboutMe;
