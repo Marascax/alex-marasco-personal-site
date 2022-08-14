5afdc9e2fe898a6d15919eb5be14a9d3fab62cc0
AGMarasco <alexmarasco13@gmail.com>
2022-08-14 19:48:59 -0400

    contact info div grouping

--- src/components/aboutMeBody.js
+++ src/components/aboutMeBody.js
@@ -7,19 +7,22 @@ const AboutMeBody = props => {
             
             <p>{pageData.text}</p>
 
-            <h2>Contact Information</h2>
-            <p>
-                E-mail: <a href={`mailto:${pageData.contactInfo.email}`}>{pageData.contactInfo.email}</a>
-            </p>
-            <p>
-                Phone Number: {pageData.contactInfo.phoneNumber}
-            </p>
-            <p className='LinkedIn'>
-                <a href={pageData.contactInfo.linkedIn}>
-                    <img className='Icon' src='./images/linkedin-icon.webp' alt='LinkedIn Icon'/>
-                </a>
-            </p>
+            <div className='ContactInfo'>
 
+                <h2>Contact Information</h2>
+                <p>
+                    E-mail: <a href={`mailto:${pageData.contactInfo.email}`}>{pageData.contactInfo.email}</a>
+                </p>
+                <p>
+                    Phone Number: {pageData.contactInfo.phoneNumber}
+                </p>
+                <p className='LinkedIn'>
+                    <a href={pageData.contactInfo.linkedIn}>
+                        <img className='Icon' src='./images/linkedin-icon.webp' alt='LinkedIn Icon'/>
+                    </a>
+                </p>
+
+            </div>
         </div>
     )
 }
