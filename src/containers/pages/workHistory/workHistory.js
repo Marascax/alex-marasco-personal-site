b9d5cc3785b1aef36506999776ab82decb6de612
AGMarasco <alexmarasco13@gmail.com>
2022-09-03 13:53:35 -0400

    timeline with line, dots per item

--- src/containers/pages/workHistory/workHistory.js
+++ src/containers/pages/workHistory/workHistory.js
@@ -21,12 +21,31 @@ const WorkHistory = props => {
                 <div className='timeline'>
 
                     <div className='timeline-item'>
-                        
 
                         <div className='item-container'>
                             <div className='item-title'>General Dynamics Information Technology</div>
                             <div className='item-subtitle'>Technical Intern</div>
                             <div className='item-date'>Jun 2020 - May 2021</div>
+                        </div>
+                        
+                    </div>
+
+                    <div className='timeline-item'>
+
+                        <div className='item-container'>
+                            <div className='item-title'>CGI</div>
+                            <div className='item-subtitle'>Software Developer</div>
+                            <div className='item-date'>Jun 2021 - Apr 2022</div>
+                        </div>
+                        
+                    </div>
+
+                    <div className='timeline-item'>
+
+                        <div className='item-container'>
+                            <div className='item-title'>Title3</div>
+                            <div className='item-subtitle'>Subtitle3</div>
+                            <div className='item-date'>Date3</div>
                         </div>
                         
                     </div>
