08fe6f30e1123fcf46e1e6128807918623a26115
AGMarasco <alexmarasco13@gmail.com>
2022-09-03 13:35:40 -0400

    timeline item with title, subtitle, and date

--- src/containers/pages/workHistory/workHistory.js
+++ src/containers/pages/workHistory/workHistory.js
@@ -1,4 +1,3 @@
-
 import { useAppContext } from '../../../lib/appContext';
 
 import './workHistory.scss'
@@ -8,14 +7,31 @@ const WorkHistory = props => {
     const context = useAppContext();
 
     return (
-        <div className='WorkHistory'>
+        <div className='work-history'>
 
             <div className='Header'>
                 <h1>Work History</h1>
             </div>
 
-            <div className='WorkHistoryContent'>
-                
+            <div className='work-history-content'>
+                <div className='work-history-selection'>
+
+                </div>
+
+                <div className='timeline'>
+
+                    <div className='timeline-item'>
+                        
+
+                        <div className='item-container'>
+                            <div className='item-title'>General Dynamics Information Technology</div>
+                            <div className='item-subtitle'>Technical Intern</div>
+                            <div className='item-date'>Jun 2020 - May 2021</div>
+                        </div>
+                        
+                    </div>
+
+                </div>
             </div>
 
         </div>
