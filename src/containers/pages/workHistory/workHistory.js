2f16715b4b0c8cfa9c53ea6aa06eb234760ae1b3
AGMarasco <alexmarasco13@gmail.com>
2022-09-03 14:06:14 -0400

    load work history from file

--- src/containers/pages/workHistory/workHistory.js
+++ src/containers/pages/workHistory/workHistory.js
@@ -1,3 +1,6 @@
+import { useState } from 'react';
+
+import Loading from '../../../components/loading';
 import { useAppContext } from '../../../lib/appContext';
 
 import './workHistory.scss'
@@ -5,6 +8,35 @@ import './workHistory.scss'
 const WorkHistory = props => {
 
     const context = useAppContext();
+
+    const [pageJson, setPageJson] = useState(null);
+
+    let content;
+    if (!pageJson) content = <Loading/>;
+    else {
+        const pageJobs = pageJson.jobs;
+        // create timeline item for each job
+        content = pageJobs.map((job, index) => (
+            <div className='timeline-item' key={index}>
+
+                <div className='item-container'>
+                    <div className='item-title'>{job.company}</div>
+                    <div className='item-subtitle'>{job.title}</div>
+                    <div className='item-date'>{job.startDate} - {job.endDate}</div>
+                </div>
+                
+            </div>
+        ));
+    }
+
+    if (!pageJson) { // if page json hasn't been loaded
+        // fetch the doc
+        fetch('./data/WorkHistory.json')
+            .then(response => response.json())
+            .then(json => {
+                setPageJson(json);
+            });
+    }
 
     return (
         <div className='work-history'>
@@ -14,42 +46,12 @@ const WorkHistory = props => {
             </div>
 
             <div className='work-history-content'>
-                <div className='work-history-selection'>
+                <div className='work-history-selection' style={{ display: !pageJson ? 'none' : 'block' }}>
 
                 </div>
 
-                <div className='timeline'>
-
-                    <div className='timeline-item'>
-
-                        <div className='item-container'>
-                            <div className='item-title'>General Dynamics Information Technology</div>
-                            <div className='item-subtitle'>Technical Intern</div>
-                            <div className='item-date'>Jun 2020 - May 2021</div>
-                        </div>
-                        
-                    </div>
-
-                    <div className='timeline-item'>
-
-                        <div className='item-container'>
-                            <div className='item-title'>CGI</div>
-                            <div className='item-subtitle'>Software Developer</div>
-                            <div className='item-date'>Jun 2021 - Apr 2022</div>
-                        </div>
-                        
-                    </div>
-
-                    <div className='timeline-item'>
-
-                        <div className='item-container'>
-                            <div className='item-title'>Title3</div>
-                            <div className='item-subtitle'>Subtitle3</div>
-                            <div className='item-date'>Date3</div>
-                        </div>
-                        
-                    </div>
-
+                <div className='timeline' style={{ display: !pageJson ? 'none' : 'block' }}>
+                    {content}
                 </div>
             </div>
 
